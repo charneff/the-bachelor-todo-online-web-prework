b433347e94f8bfe13adfbd3a7b941b67b2b65d4a
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-08 15:37:37 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,8 @@ def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant_hash|
     if contestant_hash["status"].downcase == "winner"
       return contestant_hash["name"].split(" ").first
-      end
+    end
+  end
   # code here
 end
 
